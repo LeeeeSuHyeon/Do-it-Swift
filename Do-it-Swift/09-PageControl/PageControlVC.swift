ffbcfae0e88e3a8ffc770c556826c2991600f05f
suhyeon lee <tngus0673@naver.com>
2024-07-01 15:47:33 +0900

    09. Page Control

--- Do-it-Swift/09-PageControl/PageControlVC.swift
+++ Do-it-Swift/09-PageControl/PageControlVC.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 class PageControlVC : UIViewController {
+    
+    var images = ["MuhanEnd.png", "couple_01-1.png", "MuhanMiddle.png", "MuhanStart.png"]
+    
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
         
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = .green
+        pageControl.currentPageIndicatorTintColor = .red
+        imgView.image = UIImage(named: images[0])
+    }
+    
+    
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named: images[pageControl.currentPage])
     }
 }
