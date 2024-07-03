2f68d39ba6ce4a3c3d3894a8a8f9d9395d76b733
suhyeon lee <tngus0673@naver.com>
2024-07-03 15:55:05 +0900

    12. 도전 문제

--- Do-it-Swift/12-TableView/AddViewController.swift
+++ Do-it-Swift/12-TableView/AddViewController.swift
@@ -7,9 +7,13 @@
 
 import UIKit
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
 
     @IBOutlet var tfAddItem: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pickerImage: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,6 +28,28 @@ class AddViewController: UIViewController {
         _ = navigationController?.popViewController(animated: true)
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return items.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image : UIImage(named: itemsImageFile[row]))
+        imageView.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        return imageView
+                                    
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        imageView.image = UIImage(named: itemsImageFile[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return CGFloat(50.0)
+    }
     /*
     // MARK: - Navigation
 
