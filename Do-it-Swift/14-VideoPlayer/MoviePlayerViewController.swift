e52b20ff56f5bfb4c948118518d391eb5c351318
suhyeon lee <tngus0673@naver.com>
2024-07-06 18:18:24 +0900

    14. 비디오 재생 앱

---
+++ Do-it-Swift/14-VideoPlayer/MoviePlayerViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MoviePlayerViewController.swift
+//  Do-it-Swift
+//
+//  Created by 이수현 on 7/6/24.
+//
+
+import UIKit
+import AVKit
+
+
+class MoviePlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // 내부 비디오 재생
+    @IBAction func btnPlayInternalMovie(_ sender: UIButton) {
+        // 내부 파일 mp4
+        let filePath : String? = Bundle.main.path(forResource: "FastTyping", ofType: "mp4")
+        let url = NSURL(fileURLWithPath: filePath!)
+        
+        playVideo(url: url)
+    }
+    
+    
+    // 외부 비디오 재생
+    @IBAction func btnPlayExternalMovie(_ sender: UIButton) {
+        // 외부 파일 mp4
+        let url = NSURL(string: "http://dl.dropboxusercontent.com/s/e38auz050w2mvud/Fireworks.mp4")!
+        
+        playVideo(url: url)
+        
+
+    }
+    
+    private func playVideo(url : NSURL){
+        let playerController = AVPlayerViewController()
+        
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        
+        self.present(playerController, animated: true){
+            player.play()
+        }
+    }
+
+}
