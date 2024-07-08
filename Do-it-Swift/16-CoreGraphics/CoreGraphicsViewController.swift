c602cd33d629ac0ca2abd7cb0667120a818c6b30
suhyeon lee <tngus0673@naver.com>
2024-07-08 15:29:17 +0900

    16. Core Graphics Storyboard

---
+++ Do-it-Swift/16-CoreGraphics/CoreGraphicsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CoreGraphicsViewController.swift
+//  Do-it-Swift
+//
+//  Created by 이수현 on 7/8/24.
+//
+
+import UIKit
+
+class CoreGraphicsViewController: UIViewController {
+
+    @IBOutlet weak var imgView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func btnDrawLine(_ sender: UIButton) {
+    }
+    @IBAction func btnDrawRectangle(_ sender: UIButton) {
+    }
+    @IBAction func btnDrawCircle(_ sender: UIButton) {
+    }
+    @IBAction func btnDrawArc(_ sender: UIButton) {
+    }
+    @IBAction func btnDrawFill(_ sender: UIButton) {
+    }
+}
